2447dc59063158a88c499302c7534cdddcb15f83
dezminator <dezminator@gmail.com>
2021-07-20 14:06:44 +0200

    test

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world! with flavour.
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Shows the java build works.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Java pipeline works!");
+    }
+}
